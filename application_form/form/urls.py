1203a9708c184dc069302e931e9523b43d8f85dd
Rishikesh Sharma <wrongdirection72@gmail.com>
2024-12-04 16:42:17 +0545

    serve pdf as static file

--- application_form/form/urls.py
+++ application_form/form/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path,url
 from . import views
 
 urlpatterns = [
     path('', views.employee_application_form, name='employee_application_form'),  # Employee application form page
     path('success/', views.application_success, name='application_success'),
-    path('download/<id>/',views.download_pdf,name='download_pdf')  
+    path('download/<id>/',views.download_pdf,name='download_pdf')  ,
+    url(r'^download/(\d+)/$', 'form.views.download_pdf', name='pdf_by_id'),
 ]
--- application_form/form/views.py
+++ application_form/form/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render, redirect
 from .forms import EmployeeApplicationForm
 from django.template.loader import render_to_string
 from form.models import Form
-
+from django.template.loader import get_template
+from django.http import HttpResponse
 from xhtml2pdf import pisa
+
+import pdfkit
 
 
 def employee_application_form(request):
     if request.method == "POST":
         form = EmployeeApplicationForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('application_success')
+            saved_form=  form.save()
+            # return redirect(request,'application_success',{"data":saved_form})
+            return render(request, 'form_app/success.html',{"data":saved_form})
+
     else:
         form = EmployeeApplicationForm()
     return render(request, 'form_app/employee_application_form.html', {'form': form})
@@ -19,27 +24,27 @@ def employee_application_form(request):
 def application_success(request):
     return render(request, 'form_app/success.html')
 
+
 def download_pdf(request,id):
-    
-     application_data = Form.objects.get(pk=id)
-     rendered = render_to_string('form_app/application_form_filled.html',{'data':application_data})
+    application_data = Form.objects.get(pk=id)
+    rendered = render_to_string('form_app/application_form_filled.html',{'data':application_data})
     #  print(rendered)   
-
      # enable logging
-     pisa.showLogging()
-
-     pdf_path = "static/application_pdf/"+id+".pdf"
-     with open(pdf_path, "w+b") as result_file:
-        # convert HTML to PDF
-        pisa_status = pisa.CreatePDF(
-            rendered,       
-            dest=result_file,  
-        )
-
+    pisa.showLogging()
+    pdf_path = "static/application_pdf/"+id+".pdf"
+    with open(pdf_path, "w+b") as result_file:
+    # convert HTML to PDF
+     pisa_status = pisa.CreatePDF(
+        rendered,       
+        dest=result_file,  
+    )
     # Check for errors
-     if pisa_status.err:
-        print("An error occurred!")                                         
+    if pisa_status.err:
+     print("An error occurred!")                                         
 
-     return render(request, 'form_app/success.html')
+    redirect_url="/static/application_pdf/"+id+".pdf/"
+    return redirect(redirect_url)
+    # return redirect(pdf_path)
+    # return render(request, 'form_app/success.html')
 
     
